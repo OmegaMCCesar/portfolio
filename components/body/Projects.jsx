4a338ecdd03e38d0b087b26f75d6d968d2544a2c
Cesar <luiscesar.munoz.cervantes.upiit@gmail.com>
2023-04-21 00:44:38 -0600

    second push

--- components/body/Projects.jsx
+++ components/body/Projects.jsx
@@ -1,10 +1,14 @@
 import styles from './Projects.module.css'
+const projects = [{img:'./imgExc.png',url:'https://eccellenza.online'},{img:'./omegap.png',url:'https://omegamc.vercel.app'}]
 
 const Projects = () => {
   return <div className={styles.container}>
-  <h1>
+  <h1 className={styles.title}>
 Projects
   </h1>
+  <div>
+    {projects.map(p =><a href={p.url}> <img src={p.img} alt={p.img} className={styles.img}/></a>)}
+  </div>
   </div>
 }
 
